df1fbaef7453a64263fa343c764805205f13c4fd
nagisaShiyoto <elon.gilad9@gmail.com>
2025-06-17 13:04:52 +0300

    added a program to calc the root of a positive number

---
+++ groot/groot/groot.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+#define POSITIVE_THRESHOLD 0
+
+float get_positive_number();
+
+int main() {
+    float number = get_positive_number();
+    float result = std::sqrt(number);
+    std::cout << "result: " << result << std::endl;
+
+    return 0;
+}
+
+float get_positive_number() {
+    float input = -1;
+    while (input < POSITIVE_THRESHOLD) {
+        std::cout << "enter positive number: ";
+        std::cin >> input;
+    }
+    return input;
+}
