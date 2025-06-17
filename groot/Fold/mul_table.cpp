f052c2f9d2abac517ab8d14d35fa4d18ef204756
nagisaShiyoto <elon.gilad9@gmail.com>
2025-06-17 19:53:06 +0300

    changed it to log 10 and change to camelCase

--- groot/Fold/mul_table.cpp
+++ groot/Fold/mul_table.cpp
@@ -4,34 +4,23 @@
 void printMultiplyTable(int startValue, int endValue) {
     int result = 0;
     // the largest number has the largest digit amount so he also have the largest needed space to print
-    int highestSpaceAmount = digitAmount(endValue * endValue) + 1;
+    int highestSpaceAmount = static_cast<int>(std::log10(endValue * endValue)) + 1;
 
     for (int i = startValue; i <= endValue; i++) {
         for (int j = startValue; j <= endValue; j++) {
             result = i * j;
             std::cout << result;
-            printSpaces(highestSpaceAmount, result);
+            printPaddingSpaces(highestSpaceAmount, result);
         }
         std::cout << std::endl;
     }
 }
 
 
-void printSpaces(int highestSpaceNumber, int result) {
-    int spaceTaken = digitAmount(result);
-    int space_amount = highestSpaceNumber - spaceTaken;
-    for (int i = 0; i < space_amount; i++) {
+void printPaddingSpaces(int highestSpaceNumber, int result) {
+    int spaceTaken = static_cast<int>(std::log10(result));
+    //int spaceAmount = highestSpaceNumber - spaceTaken;
+    for (int i = spaceTaken; i < highestSpaceNumber; i++) {
         std::cout << " ";
     }
-}
-
-
-int digitAmount(int result) {
-    int counter = 0;
-
-    while (result > 0) {
-        counter++;
-        result /= 10;
-    }
-    return counter;
 }
